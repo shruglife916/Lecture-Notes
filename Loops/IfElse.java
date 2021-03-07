b9ca01abce1500eb005840ef4dfb45d531b6c717
Jerome Hall <shruglife916@gmail.com>
2021-03-07 17:58:14 -0600

    Loops

---
+++ Loops/IfElse.java
@@ -0,0 +1,14 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int time = 20;
+		if (time > 19) {
+		  System.out.println("Good night.");
+		} else 
+		  System.out.println("Good evening.");
+		}
+
+	}
+
+
