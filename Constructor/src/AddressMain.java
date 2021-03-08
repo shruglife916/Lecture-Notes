830b79fffb01174d7eb13bae3bad6f92f1ce8853
Jerome Hall <shruglife916@gmail.com>
2021-03-07 18:00:21 -0600

    Deletes Constructor

--- Constructor/src/AddressMain.java
+++
@@ -1,9 +0,0 @@
-
-public class AddressMain {
-
-	public static void main(String[] args) {
-		Address a1 = new Address(14,"Wall Street","New York","New York",10005);
-			System.out.println(a1);
-	}
-
-}
